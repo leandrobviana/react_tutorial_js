9efe844a3bdd47b16bd333d6ad1057f63f7223bb
EMBAD\LBVIANA <leandro.viana@embraer.com.br>
2018-09-19 15:29:56 -0300

    second

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
-  };
+  //can use constructor instead of arrow function
+  /* constructor() {
+    super();
+    //this.handleIncrement = this.handleIncrement.bind(this);
+  } */
+
+  //removed to have a single source of truth, so we dont need the incremented here,
+  //just in the parent component
+  /*   handleIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  }; */
 
   render() {
+    /* {this.props.children} inside return to pass as children */
+    console.log(this.props);
+    let product = { id: 1 };
+    /* () => this.handleIncrement(product) */
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          //onClick={}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
         <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
@@ -22,13 +45,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.state;
+    return value === 0 ? 'Zero' : value;
   }
 }
 
